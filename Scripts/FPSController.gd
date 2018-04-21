6b1b6e8d64386fb35775a30869e5d1e391718ac1
Victor Lima <jvlpark3@outlook.com>
2018-04-21 15:10:45 -0300

    Adding base mouse controller and fixing input map and size of the character
    Adding base mouse controller and fixing input map and size of the character

--- Scripts/FPSController.gd
+++ Scripts/FPSController.gd
@@ -5,37 +5,56 @@ extends KinematicBody
 # var b = "textvar"
 
 var direction = Vector3()
-export var speed = 15
+export var speed = 30
 export var gravity = 3
 var dir = Vector3(0,0,0)
 export var jump = 10
+export var mouse_sensitivity = 0.3
+var camera_angle = 0
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	set_process_input(true)
 	pass
 
 func _physics_process(delta):
 	
 	direction = Vector3(0,0,0)
-	if Input.is_action_pressed("up"):
-		direction.z = -1
-	if Input.is_action_pressed("down"):
-		direction.z = 1
-	if Input.is_action_pressed("rigth"):
-		direction.x = 1
-	if Input.is_action_pressed("left"):
-		direction.x = -1
+	var aim = $Head/Camera.get_global_transform().basis
+	if Input.is_action_pressed("ui_up"):
+		direction -= aim.z
+	if Input.is_action_pressed("ui_down"):
+		direction += aim.z
+	if Input.is_action_pressed("ui_right"):
+		direction += aim.x
+	if Input.is_action_pressed("ui_left"):
+		direction -= aim.x
 	
-	direction = direction * speed
+	direction = direction.normalized()
+	direction *= speed
+	dir = direction.linear_interpolate(direction, speed * delta)
 	
 	dir.y += -9.8 * delta * gravity
 	dir.x = direction.x
 	dir.z = direction.z
 	
 	dir = move_and_slide(dir, Vector3(0,1,0))
-	
-	if Input.is_action_pressed("space") && is_on_floor():
-		## Isso aqui faz ele pular
+
+	if Input.is_action_pressed("ui_select") && is_on_floor():
 		dir.y = jump
 	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		$Head.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		var change = -event.relative.y * mouse_sensitivity
+		
+		if (change + camera_angle) < 90 and (change + camera_angle) > -90:
+			$Head/Camera.rotate_x(deg2rad(change))
+			camera_angle += change
+
+
+
+
+
+
+		
