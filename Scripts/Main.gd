f11bd276cf50b2400d4872102b9fa3f7fcfc5f32
Linko-3D <dbekhouc@gmail.com>
2018-04-22 01:06:45 +0200

    fix

--- Scripts/Main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _ready():
-	Game.player = $FPSController
-	
-func _process(delta):
-	if Input.is_action_pressed("quit"): 
-    	get_tree().quit() 
--- Scripts/Zombie.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody
-
-func _process(delta): 
-  self.look_at(Game.player.translation, Vector3(0,1,0)) 
-  var dir = Game.player.translation - self.translation 
-  self.apply_impulse(Vector3(0,0,0), dir.normalized())
