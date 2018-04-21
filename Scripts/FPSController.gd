2be7b21f6a4b13acb19cc660efda1825c918361c
Victor Lima <jvlpark3@outlook.com>
2018-04-21 15:36:00 -0300

    Finishing the mouse controller
    Finishing the mouse controller

--- Scripts/FPSController.gd
+++ Scripts/FPSController.gd
@@ -4,13 +4,24 @@ extends KinematicBody
 # var a = 2
 # var b = "textvar"
 
+
+# Helpers
 var direction = Vector3()
-export var speed = 30
-export var gravity = 3
-var dir = Vector3(0,0,0)
-export var jump = 10
+var velocity = Vector3(0,0,0)
+
+#walk variables
+const MAX_SPEED = 20
+const MAX_RUNNING_SPEED = 30
+const ACCEL = 2
+const DEACCEL = 6
+export var gravity = -9.8 * 3
+
+#Mouse controller variables
 export var mouse_sensitivity = 0.3
 var camera_angle = 0
+
+#jump
+export var jump = 10
 
 func _ready():
 	set_process_input(true)
@@ -29,18 +40,40 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		direction -= aim.x
 	
+	direction.y = 0
 	direction = direction.normalized()
-	direction *= speed
-	dir = direction.linear_interpolate(direction, speed * delta)
+	velocity.y += gravity * delta
 	
-	dir.y += -9.8 * delta * gravity
-	dir.x = direction.x
-	dir.z = direction.z
+	var temp_velocity = velocity
+	temp_velocity.y = 0
 	
-	dir = move_and_slide(dir, Vector3(0,1,0))
+	var speed
+	if Input.is_action_pressed("shift"):
+		speed = MAX_RUNNING_SPEED
+	else:
+		speed = MAX_SPEED
+	
+	# where would the player go at max speed
+	var target = direction * speed
+	
+	var acceleration
+	if direction.dot(temp_velocity) > 0:
+		acceleration = ACCEL
+	else:
+		acceleration = DEACCEL
+	
+	# calculate a portion of the distance to go
+	temp_velocity = temp_velocity.linear_interpolate(target, acceleration * delta)
+	
+	velocity.x = temp_velocity.x
+	velocity.z = temp_velocity.z
+	
 
+	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+	
 	if Input.is_action_pressed("ui_select") && is_on_floor():
-		dir.y = jump
+		velocity.y = jump
+
 	pass
 
 func _input(event):
