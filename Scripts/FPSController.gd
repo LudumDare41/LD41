3d73dde03068b94e370901e8081df2f3873858a1
Victor Lima <jvlpark3@outlook.com>
2018-04-22 16:32:40 -0300

    Fix the commit mess :D
    Fix the commit mess :D

---
+++ Scripts/FPSController.gd
@@ -0,0 +1,84 @@
+extends KinematicBody
+
+# Helpers
+var direction = Vector3()
+var velocity = Vector3(0,0,0)
+
+#walk variables
+const MAX_SPEED = 12
+const MAX_RUNNING_SPEED = 24
+const ACCEL = 2
+const DEACCEL = 8
+export var gravity = -9.8 * 3
+
+#Mouse controller variables
+export var mouse_sensitivity = 0.3
+var camera_angle = 0
+
+#jump
+export var jump = 10
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#register the player to the global class
+	Game.player = self
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
+	
+	direction = Vector3(0,0,0)
+	var aim = $Head/Camera.get_global_transform().basis
+	if Input.is_action_pressed("ui_up"):
+		direction -= aim.z
+	if Input.is_action_pressed("ui_down"):
+		direction += aim.z
+	if Input.is_action_pressed("ui_right"):
+		direction += aim.x
+	if Input.is_action_pressed("ui_left"):
+		direction -= aim.x
+	
+	direction.y = 0
+	direction = direction.normalized()
+	velocity.y += gravity * delta
+	
+	var temp_velocity = velocity
+	temp_velocity.y = 0
+	
+	var speed
+	if Input.is_action_pressed("shift"):
+		speed = MAX_RUNNING_SPEED
+	else:
+		speed = MAX_SPEED
+	
+	# where would the player go at max speed
+	var target = direction * speed
+	
+	var acceleration
+	if direction.dot(temp_velocity) > 0:
+		acceleration = ACCEL
+	else:
+		acceleration = DEACCEL
+	
+	# calculate a portion of the distance to go
+	temp_velocity = temp_velocity.linear_interpolate(target, acceleration * delta)
+	
+	velocity.x = temp_velocity.x
+	velocity.z = temp_velocity.z
+	
+
+	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+	
+	if Input.is_action_pressed("ui_select") && is_on_floor():
+		velocity.y = jump
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		$Head.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		var change = -event.relative.y * mouse_sensitivity
+		
+		if (change + camera_angle) < 90 and (change + camera_angle) > -90:
+			$Head/Camera.rotate_x(deg2rad(change))
+			camera_angle += change
---
+++ Scripts/Weapons/Weapons.gd
@@ -0,0 +1,129 @@
+extends Spatial
+
+#Shooting params
+var shoot_range = 1000
+var screen_width_center = 0
+var screen_height_center = 0
+var shoot_origin = Vector3()
+var shoot_direction = Vector3()
+var shooting = false
+var impactForce = 20
+var weaponID = 0
+
+# Pistol
+var PistolData = {
+	magSize = 30,
+	bulletsInWeapon = 10,
+	bulletsOutWeapon = 100
+}
+
+#Effects
+onready var particleSystem = get_node("Particles")
+onready var audioSystem = get_node("Audio")
+onready var ReloadaudioSystem = get_node("ReloadAudio")
+onready var PickUpAudio = get_node("PickUpAudio")
+
+#Animation
+onready var animationPlayer = get_node("Handgun/AnimationPlayer")
+var walk = false
+var idle = true
+
+func _ready():
+	updateAmmoUI(PistolData)
+	screen_width_center = OS.get_window_size().x/2
+	screen_height_center = OS.get_window_size().y/2
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var camera = get_node("../")
+		shoot_origin = camera.project_ray_origin(Vector2(screen_width_center, screen_height_center))
+		shoot_direction = camera.project_ray_normal(Vector2(screen_width_center, screen_height_center)) * shoot_range
+		
+		if event.button_index == 1:
+			shooting = true
+func _physics_process(delta):
+	animations()
+	if shooting and !animationPlayer.get_current_animation() == "reload":
+		pistol()
+	shooting = false
+	if Input.is_action_pressed("reload"):
+		reload(PistolData)
+	pass
+
+func pistol():
+	if PistolData.bulletsInWeapon > 0:
+		particleSystem.restart()
+		audioSystem.play()
+		var impulse
+		var impact_position
+		
+		var space_state = get_world().direct_space_state
+		var result = space_state.intersect_ray(shoot_origin, shoot_direction, [self], 1)
+		
+		PistolData.bulletsInWeapon -= 1
+		animationPlayer.play("fire")
+		animationPlayer.queue("idle (copy)")
+		updateAmmoUI(PistolData)
+		if result:
+			impulse = (result.position - global_transform.origin).normalized()
+			var position = result.position - result.collider.global_transform.origin
+			if shooting:
+				# for objects
+				if result.collider is RigidBody:
+					result.collider.apply_impulse(position, impulse*impactForce)
+				# for zombie
+				if result.collider is KinematicBody and result.collider.has_method("hit"):
+					result.collider.hit(20, position)
+			
+				
+func reload(weapon):
+	if weapon.bulletsOutWeapon > 0 and weapon.bulletsInWeapon < weapon.magSize and !animationPlayer.get_current_animation() == "fire":
+		ReloadaudioSystem.play()
+		animationPlayer.play("reload")
+		animationPlayer.queue("idle (copy)")
+		var reloadRange = weapon.magSize - weapon.bulletsInWeapon
+		var reloadValue
+		
+		if weapon.bulletsOutWeapon >= reloadRange:
+			reloadValue = reloadRange
+		else:
+			reloadValue = weapon.bulletsOutWeapon
+			
+		weapon.bulletsOutWeapon -= reloadValue
+		weapon.bulletsInWeapon += reloadValue
+		updateAmmoUI(weapon)
+	
+func updateAmmoUI(weapon):
+	var inWeaponLabel = get_node("../../../Control/AmmoUI/InWeapon")
+	inWeaponLabel.text = str(weapon.bulletsInWeapon)
+	var inMagLabel = get_node("../../../Control/AmmoUI/InMag")
+	inMagLabel.text = str(weapon.bulletsOutWeapon)
+
+func animations():
+	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down") or Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left"):
+		if walk or animationPlayer.get_current_animation() == "fire" or animationPlayer.get_current_animation() == "reload":
+			pass
+		else:
+			animationPlayer.play("walk (copy)")
+			walk = true
+			idle = false
+	else:
+		if idle or animationPlayer.get_current_animation() == "fire" or animationPlayer.get_current_animation() == "reload":
+			pass
+		else:
+			animationPlayer.play("idle (copy)")
+			idle = true
+			walk = false
+
+func _on_Area_area_entered(area):
+	
+	if area.is_in_group("pistolAmmo"):
+		getAmmo(PistolData, 30)
+		updateAmmoUI(PistolData)
+		area.queue_free()
+		PickUpAudio.play()
+	pass # replace with function body
+
+func getAmmo(weapon, amount):
+	weapon.bulletsOutWeapon += amount
