23ba204b6ef76ba39564a0a37e89ce5b4701eed1
Victor Lima <jvlpark3@outlook.com>
2018-04-23 02:53:54 -0300

    Mainmenu scritp
    Mainmenu scritp

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -19,16 +19,13 @@ func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Main.tscn")
 	pass # replace with function body
 
-
 func _on_ControlsButton_pressed():
 	get_tree().change_scene("res://ControlsMenu.tscn")
 	pass # replace with function body
 
-
 func _on_ExitButton_pressed():
 	get_tree().quit()
 	pass # replace with function body
-
 
 func _on_CreditsButton_pressed():
 	get_tree().change_scene("res://CreditsMenu.tscn")
