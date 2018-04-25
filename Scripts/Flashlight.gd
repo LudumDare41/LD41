56952add1fa1fa81cc7e8c4a89276ec78a7accc5
Nutriz <jerome.gully0@gmail.com>
2018-04-25 22:52:06 +0200

    Big cleangit status!

---
+++ Scripts/Flashlight.gd
@@ -0,0 +1,30 @@
+extends SpotLight
+
+
+var default_light_energy = 0
+var flashlightStatus = true
+const MAX_BATTERY = 100
+var battery = MAX_BATTERY
+onready var batteryUI = get_node("../../../Control/FlashlightStatus/Flashlight")
+
+func _ready():
+	default_light_energy = light_energy
+
+func _process(delta):
+	if Input.is_action_just_pressed("Flashlight"):
+		flashlightStatus = !flashlightStatus
+	if flashlightStatus and battery > 1:
+		light_energy = default_light_energy
+		battery -= 5 * delta
+	else:
+		if battery < 100:
+			light_energy = 0
+			battery += 15 * delta
+		
+	if battery < 2:
+		flashlightStatus = false
+	updateStatusUI(int(battery))
+	
+func updateStatusUI(battery):
+	batteryUI.text = str(battery)
+
